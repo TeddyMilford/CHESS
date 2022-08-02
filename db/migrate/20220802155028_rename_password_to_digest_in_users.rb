82f485cfa569ed3daa844390b6e983ad8ce50f41
Teddy Milford <teddy.milford@gmail.com>
2022-08-02 09:58:06 -0600

    working

---
+++ db/migrate/20220802155028_rename_password_to_digest_in_users.rb
@@ -0,0 +1,9 @@
+class RenamePasswordToDigestInUsers < ActiveRecord::Migration[7.0]
+  def up
+    rename_column :users, :password, :password_digest
+  end
+
+  def down
+    rename_column :users, :password_digest, :password
+  end
+end
